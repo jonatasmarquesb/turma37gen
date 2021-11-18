019fa9e0327ace86cf8f0d6dd3943962c0635ab1
jonatas marques <marquesjon@gmail.com>
2021-11-18 13:43:34 -0300

    Adicionando Swagger ao Blog Pessoal

--- spring/blogpessoal/src/main/java/org/generation/blogpessoal/model/Postagem.java
+++ spring/blogpessoal/src/main/java/org/generation/blogpessoal/model/Postagem.java
@@ -14,29 +14,34 @@ import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 @Entity
 @Table(name = "postagem")
 
 public class Postagem {
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
-	
+
 	@NotNull
-	@Size(min=5,max=100)
+	@Size(min = 5, max = 100)
 	private String titulo;
-	
+
 	@NotNull
-	@Size(min=5,max=500)
+	@Size(min = 5, max = 500)
 	private String texto;
-	
+
 	@Temporal(TemporalType.TIMESTAMP)
 	private Date date = new java.sql.Date(System.currentTimeMillis());
+	
 	@ManyToOne
 	@JsonIgnoreProperties("postagem")
 	private Tema tema;
-
+	
+	@ManyToOne
+	@JsonIgnoreProperties("postagem")
+	private Usuario usuario;
 
 	public long getId() {
 		return id;
@@ -69,7 +74,7 @@ public class Postagem {
 	public void setDate(Date date) {
 		this.date = date;
 	}
-	
+
 	public Tema getTema() {
 		return tema;
 	}
--- spring/blogpessoal/src/main/java/org/generation/blogpessoal/model/Usuario.java
+++ spring/blogpessoal/src/main/java/org/generation/blogpessoal/model/Usuario.java
@@ -1,12 +1,21 @@
 package org.generation.blogpessoal.model;
 
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import io.swagger.annotations.ApiModelProperty;
 
 @Entity
 @Table(name = "usuarios")
@@ -20,13 +29,18 @@ public class Usuario {
 	@Size(min = 5, max = 100)
 	private String nome;
 
-	@NotNull
-	@Size(min = 5, max = 100)
+	@ApiModelProperty(example = "email@email.com.br")
+	@NotNull(message = "O atributo Usuário é Obrigatório!")
+	@Email(message = "O atributo Usuário deve ser um email válido!")
 	private String usuario;
 
 	@NotNull
 	@Size(min = 5)
 	private String senha;
+
+	@OneToMany(mappedBy = "usuario", cascade = CascadeType.REMOVE)
+	@JsonIgnoreProperties("usuario")
+	private List<Postagem> postagem;
 
 	public long getId() {
 		return id;
